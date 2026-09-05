c7097e6b09e509083fc6fae8fc57b8186d16b498
Clément MARTY <clement_marty@outlook.fr>
2026-09-05 16:18:06 +0200

    Implement terrain generation

--- game/constants.py
+++ game/constants.py
@@ -3,6 +3,7 @@ Constant values for the game
 """
 
 import arcade
+from enum import Enum
 
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
@@ -15,3 +16,12 @@ KEY_UP = [arcade.key.UP, arcade.key.W]
 KEY_DOWN = [arcade.key.DOWN, arcade.key.S]
 KEY_LEFT = [arcade.key.LEFT, arcade.key.A]
 KEY_RIGHT = [arcade.key.RIGHT, arcade.key.D]
+
+# Map tile ids
+class Tile(Enum):
+    WATER = 0
+    GRASS = 1
+    STONE = 2
+    SAND = 3
+    SNOW = 4
+    
---
+++ game/map_generation/terrain.py
@@ -0,0 +1,31 @@
+import noise
+import numpy as np
+
+
+class TerrainGenerator:
+
+    def __init__(self, shape: tuple[int, int], biome_config: dict) -> None:
+        self.shape = shape
+        self.biome = biome_config
+
+    def generate(self) -> np.ndarray:
+        scale: float = self.biome["scale"]
+        octaves: int = self.biome["octaves"]
+        persistence: float = self.biome["persistence"]
+        lacunarity: float = self.biome["lacunarity"]
+        tiles: dict[str, float] = self.biome["tiles"]
+
+        terrain = np.zeros(self.shape, dtype=np.int8)
+        for i in range(self.shape[0]):
+            for j in range(self.shape[1]):
+                x, y = i / scale, j / scale
+                noise_value = noise.pnoise2(
+                    x, y, octaves, persistence, lacunarity,
+                    repeatx=self.shape[0], repeaty=self.shape[1], base=0
+                )
+                for tile, threshold in sorted(list(tiles.items()), key=lambda item: item[1]):
+                    if noise_value < threshold:
+                        terrain[i][j] = tile
+                        break
+
+        return terrain
